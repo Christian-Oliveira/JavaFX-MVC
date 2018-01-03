18d8f9e39ba60a45bbec4132c1e6bd5562c543c2
Christian Oliveira <christianoliveirati@gmail.com>
2018-01-03 11:59:06 -0300

    criação da tela principal

---
+++ src/javafxmvc/controller/FXMLVBoxMainController.java
@@ -0,0 +1,27 @@
+package javafxmvc.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class FXMLVBoxMainController implements Initializable {
+
+    @FXML
+    private MenuItem cadClientes;
+    @FXML
+    private MenuItem proVendas;
+    @FXML
+    private MenuItem grafVendasMes;
+    @FXML
+    private MenuItem relProdutosEstoque;
+    @FXML
+    private MenuItem anchorPane;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
